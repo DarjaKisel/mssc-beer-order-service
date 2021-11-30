f877ed2c8d751faed4495e8a08d0f24b746560ac
dzinevich <darjakisel@gmail.com>
2021-11-30 19:25:31 +0200

    Enabled eureka client

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -2,7 +2,9 @@ package guru.sfg.beer.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class BeerOrderServiceApplication {
 
